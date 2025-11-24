1bacc2374a618cbe938aa6218127e980925df3b1
MiguelTheGit <miguel.osoriofernandez@plexus.es>
2025-11-24 11:51:48 +0100

    Added heroes-list to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HeroesListComponent } from './core/components/heroes-list/heroes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        redirectTo: 'heroes/list', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'heroes/list', 
+        component: HeroesListComponent 
+    },
+    {
+        path: '**',
+        redirectTo: 'heroes/list',
+        pathMatch: 'full'
+    },
+
+];
